a018b4eb4d3c05f5506ef5d9eda8c85d742aa6ba
Swapnil Agarwal <swapnil.agarwal@wingify.com>
2015-10-01 15:40:53 +0530

    Release 1.2.0

--- Pod/VWO.framework/Versions/A/Headers/VWO.h
+++ Pod/VWO.framework/Versions/A/Headers/VWO.h
@@ -8,9 +8,58 @@
 #import <Foundation/Foundation.h>
 
 @interface VWO : NSObject
+/**
+ *  Launch VWO
+ *  Call VWO's server asynchronously to fetch settings
+ */
++ (void)launchVWO;
+
+/**
+ *  Launch VWO
+ *  Call VWO's server asynchronously
+ *  It will call passed in code block on completion (success or error)
+ */
++ (void)launchVWOWithCallback:(void (^)(void))completionBlock;
+
+/**
+ *  Launch VWO
+ *  Call VWO's server asynchronously
+ *  It will call passed in code block on completion (success or error)
+ */
++ (void)launchVWOSynchronously;
+
+/**
+ *  Returns all the objects of all the available experiments.
+ *  User is made part of ALL the available experiments.
+ *  It is recommended to call this method only when you want to use all the objects,
+ *  as it makes user part of all the available experiments
+ */
 + (NSDictionary*)allObjects;
+
+/**
+ *  It searches all the available experiments, identifies the experiment and returns object for the specified key
+ *  User is made part of the identified experiment.
+ */
 + (id)objectForKey:(NSString*)key;
+
+/**
+ *  If any object for the specified key cannot be found, 
+ *  it returns the default object.
+ *  It is possible if an invalid key is specified OR
+ *  It is possible if internet connection is not available and a key is specified 
+ *  for which any experiment cannot be found
+ */
 + (id)objectForKey:(NSString*)key defaultObject:(id)defaultObject;
+
+/**
+ *  Triggers goal for the specified goal string
+ *  Each goal is only counted once
+ */
 + (void)markConversionForGoal:(NSString*)goal;
+
+/**
+ *  Triggers goal with the value for the specified goal string
+ *  Each goal is only counted once
+ */
 + (void)markConversionForGoal:(NSString*)goal withValue:(double)value;
 @end
--- VWO.podspec
+++ VWO.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
 s.name             = "VWO"
-s.version          = "1.1.0"
+s.version          = "1.2.0"
 s.summary          = "VWO SDK for A/B Testing iOS apps."
 
 s.description      = "VWO iOS SDK enables you to A/B test mobile apps."
@@ -8,13 +8,14 @@ s.homepage         = "http://vwo.com"
 s.license          = { :type => 'Commercial', :text => 'See http://vwo.com/terms-conditions' }
 s.author           = { 'VWO' => 'info@wingify.com' }
 s.source           = { :git => "https://github.com/agarwalswapnil/VWO-iOS-SDK.git", :tag => s.version.to_s }
- s.social_media_url = "http://twitter.com/wingify"
+s.social_media_url = "http://twitter.com/wingify"
 
 s.platform     = :ios, '7.0'
 s.requires_arc = true
 
 s.public_header_files = "Pod/VWO.framework/**/*.h"
-s.frameworks = "Foundation", "UIKit"
+s.frameworks = "MobileCoreServices", "SystemConfiguration", "JavaScriptCore"
+#s.library = "sqlite3"
 s.vendored_frameworks = "Pod/VWO.framework"
 s.xcconfig = { "OTHER_LDFLAGS" => "-ObjC" }
 end
