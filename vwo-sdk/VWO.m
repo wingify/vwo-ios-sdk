5cc8a8bd244471c71a11a08d235f0534d639427c
Kaunteya Suryawanshi <kaunteya.suryawanshi@wingify.com>
2017-06-29 18:11:52 +0530

    Refactored is Attached to debugger

--- vwo-sdk/VWO.m
+++ vwo-sdk/VWO.m
@@ -16,6 +16,7 @@
 #import <sys/types.h>
 #import <sys/sysctl.h>
 #import "VAOSDKInfo.h"
+#import "VAODeviceInfo.h"
 
 @implementation VWO
 
@@ -45,7 +46,7 @@
         
         [[VAOAPIClient sharedInstance] schedule];
 
-        if ([VWO isDebuggerAttached]) {
+        if ([VAODeviceInfo isAttachedToDebugger]) {
             [[VAOSocketClient sharedInstance] launch];
         }
         
@@ -79,41 +80,6 @@
                                                       tags:tags];
     
     [VAORavenClient setSharedClient:client];
-}
-
-/**
- * Check if the debugger is attached
- *
- * Taken from https://github.com/plausiblelabs/plcrashreporter/blob/2dd862ce049e6f43feb355308dfc710f3af54c4d/Source/Crash%20Demo/main.m#L96
- *
- * @return `YES` if the debugger is attached to the current process, `NO` otherwise
- */
-+ (BOOL)isDebuggerAttached {
-//    return YES;
-    
-    static BOOL debuggerIsAttached = NO;
-    
-    static dispatch_once_t debuggerPredicate;
-    dispatch_once(&debuggerPredicate, ^{
-        struct kinfo_proc info;
-        size_t info_size = sizeof(info);
-        int name[4];
-        
-        name[0] = CTL_KERN;
-        name[1] = KERN_PROC;
-        name[2] = KERN_PROC_PID;
-        name[3] = getpid();
-        
-        if (sysctl(name, 4, &info, &info_size, NULL, 0) == -1) {
-            VAOLog(@"ERROR: Checking for a running debugger via sysctl() failed: %s", strerror(errno));
-            debuggerIsAttached = false;
-        }
-        
-        if (!debuggerIsAttached && (info.kp_proc.p_flag & P_TRACED) != 0)
-            debuggerIsAttached = true;
-    });
-    
-    return debuggerIsAttached;
 }
 
 + (void)setOptOut:(BOOL)status {
