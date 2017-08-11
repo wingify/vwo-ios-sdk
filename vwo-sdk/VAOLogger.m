305691c91645fca49c96a81557fe8780b13d4ae8
Kaunteya Suryawanshi <kaunteya.suryawanshi@wingify.com>
2017-08-11 16:28:25 +0530

    NSLog instead of print

--- vwo-sdk/VAOLogger.m
+++ vwo-sdk/VAOLogger.m
@@ -14,7 +14,7 @@ void VAOLogInfo(NSString *format, ...) {
     va_start(argList, format);
     NSString* formattedMessage = [[NSString alloc] initWithFormat: format arguments: argList];
     va_end(argList);
-    printf("VWO info: %s\n", [formattedMessage UTF8String]);
+    NSLog(@"VWO info: %s\n", [formattedMessage UTF8String]);
 }
 
 void VAOLogWarning(NSString *format, ...) {
@@ -22,7 +22,7 @@ void VAOLogWarning(NSString *format, ...) {
     va_start(argList, format);
     NSString* formattedMessage = [[NSString alloc] initWithFormat: format arguments: argList];
     va_end(argList);
-    printf("VWO WARN: %s\n", [formattedMessage UTF8String]);
+    NSLog(@"VWO WARN: %s\n", [formattedMessage UTF8String]);
 }
 
 void VAOLogError(NSString *format, ...) {
@@ -30,7 +30,7 @@ void VAOLogError(NSString *format, ...) {
     va_start(argList, format);
     NSString* formattedMessage = [[NSString alloc] initWithFormat: format arguments: argList];
     va_end(argList);
-    printf("VWO ERR: %s\n", [formattedMessage UTF8String]);
+    NSLog(@"VWO ERR: %s\n", [formattedMessage UTF8String]);
 }
 
 void VAOLogException(NSString *format, ...) {
