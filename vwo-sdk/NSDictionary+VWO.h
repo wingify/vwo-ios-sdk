31793bff1876dd1213c44f440eae9631a58a593d
Kaunteya Suryawanshi <kaunteya.suryawanshi@wingify.com>
2017-08-16 13:19:32 +0530

    Updated Doc

--- vwo-sdk/NSDictionary+VWO.h
+++ vwo-sdk/NSDictionary+VWO.h
@@ -13,6 +13,18 @@ NS_ASSUME_NONNULL_BEGIN
 @interface NSDictionary (VWO)
 
 - (nullable NSString*)toString;
+
+/**
+ * Returns the keys that are not in dictionary but are in 'mustHaveKeys'
+ *
+ * Does mustHaveKeys - self.allKeys
+ *
+ * @["a":1 "b":2 "c":3].keysMissingFrom(@["a", "b", "c", "d"])
+ * Returns @["d"]
+ *
+ * @param mustHaveKeys List of the keys that dictionary must have
+ * @return Missing keys
+ */
 - (NSArray<NSString *>*) keysMissingFrom:(NSArray<NSString *> *)mustHaveKeys;
 
 @end
