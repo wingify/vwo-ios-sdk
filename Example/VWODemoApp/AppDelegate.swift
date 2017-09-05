4c60c618ae96097e9fe8884e80f7c2abf00ce14a
Kaunteya Suryawanshi <kaunteya.suryawanshi@wingify.com>
2017-09-05 16:29:47 +0530

    Added debug log to demo app

--- Example/VWODemoApp/AppDelegate.swift
+++ Example/VWODemoApp/AppDelegate.swift
@@ -22,7 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
         createSlideViewController()
-
+        VWO.logLevel = .debug
         if let apiKey = UserDefaults.standard.string(forKey: keyVWOApiKey) {
             launchVWO(apiKey)
         }
