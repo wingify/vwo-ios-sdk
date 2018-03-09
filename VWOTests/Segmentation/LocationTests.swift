54214deae68029dd9a9fd650699396551462d2e5
Kaunteya Suryawanshi <kaunteya.suryawanshi@wingify.com>
2018-03-09 13:22:53 +0530

    Added location test cases

---
+++ VWOTests/Segmentation/LocationTests.swift
@@ -0,0 +1,54 @@
+//
+//  LocationTests.swift
+//  VWOTests
+//
+//  Created by Kaunteya Suryawanshi on 09/03/18.
+//  Copyright © 2018 vwo. All rights reserved.
+//
+
+import XCTest
+extension Locale {
+    static func from(countryCode: String) -> Locale {
+        let id = Locale.identifier(fromComponents: [NSLocale.Key.countryCode._rawValue as String : countryCode])
+        return Locale(identifier: id)
+    }
+}
+class LocationTests: XCTestCase {
+
+    func testEqual() {
+        let json: [String : Any] = [
+            "type": "custom",
+            "partialSegments": [[ "type": "5", "operator": 11, "rOperandValue": ["AL", "AI" ]]]
+        ]
+
+        let evaluator = VWOSegmentEvaluator()
+
+        evaluator.locale = Locale.from(countryCode: "AL")
+        XCTAssert(evaluator.canUserBePartOfCampaign(forSegment: json), json.segmentDescription)
+
+        evaluator.locale = Locale.from(countryCode: "AI")
+        XCTAssert(evaluator.canUserBePartOfCampaign(forSegment: json), json.segmentDescription)
+
+        evaluator.locale = Locale.from(countryCode: "US")
+        XCTAssertFalse(evaluator.canUserBePartOfCampaign(forSegment: json), json.segmentDescription)
+    }
+
+    func testNotEqual() {
+        let json: [String : Any] = [
+            "type": "custom",
+            "partialSegments": [[ "type": "5", "operator": 12, "rOperandValue": ["AU", "IN", "US"]]]
+        ]
+
+        let evaluator = VWOSegmentEvaluator()
+
+        evaluator.locale = Locale.from(countryCode: "AR")
+        XCTAssert(evaluator.canUserBePartOfCampaign(forSegment: json), json.segmentDescription)
+
+        evaluator.locale = Locale.from(countryCode: "AU")
+        XCTAssertFalse(evaluator.canUserBePartOfCampaign(forSegment: json), json.segmentDescription)
+
+        evaluator.locale = Locale.from(countryCode: "IN")
+        XCTAssertFalse(evaluator.canUserBePartOfCampaign(forSegment: json), json.segmentDescription)
+
+    }
+}
